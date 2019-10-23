c4625d31f7f9c8040a3103060b9128525c9b5e89
Andy Harris <aharris@cs.iupui.edu>
2019-10-23 12:53:09 -0400

    change description in abstractions - GasPowered to be more accurate

--- abstraction/GasPowered.java
+++ abstraction/GasPowered.java
@@ -1,6 +1,6 @@
 
 public interface GasPowered{
-  // interestingly, we have three fields that
+  // interestingly, we have three methods that
   // all manipulate the gas tank, but there are
   // no variables to represent the amount of gas
   // nor the tank size.  These details are left
