9e4b648881084f31e2b732bf9ae097b08e0b8f60
Andy Harris <aharris@cs.iupui.edu>
2019-10-23 13:16:58 -0400

    makefiles in abstraction

---
+++ abstraction/java/Vehicle.java
@@ -0,0 +1,20 @@
+public abstract class Vehicle {
+  int numberOfWheels = 4;
+  
+  public Vehicle(){
+    
+  }
+
+  public abstract void steerLeft();
+  public abstract void steerRight();
+
+  public void speedUp(){
+    System.out.println("VROOM!");
+  } // end speedup
+
+  public void slowDown(){
+    System.out.println("SCREEEECH!");
+  } // end slowDown
+
+} // end Vehicle class
+
